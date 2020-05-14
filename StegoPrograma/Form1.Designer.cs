5f41c858902148aa4fd516e9cb7182be63372f03
Adrian0207 <adrian69oliva@gmail.com>
2020-05-14 11:15:54 -0500

    CommitInicial2

---
+++ StegoPrograma/Form1.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace StegoPrograma
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.btnTexto = new System.Windows.Forms.Button();
+            this.btnImagen = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtImagen = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTexto = new System.Windows.Forms.TextBox();
+            this.rtbTextoOculto = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnOcultar = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnImagen2 = new System.Windows.Forms.Button();
+            this.txtImagen2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.rtbRecuperar = new System.Windows.Forms.RichTextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.btnRecuperar = new System.Windows.Forms.Button();
+            this.ofdImagen = new System.Windows.Forms.OpenFileDialog();
+            this.ofdTexto = new System.Windows.Forms.OpenFileDialog();
+            this.sfdOcultar = new System.Windows.Forms.SaveFileDialog();
+            this.lblCapacidad = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(27, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(478, 558);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.lblCapacidad);
+            this.tabPage1.Controls.Add(this.btnTexto);
+            this.tabPage1.Controls.Add(this.btnImagen);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.txtImagen);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.txtTexto);
+            this.tabPage1.Controls.Add(this.rtbTextoOculto);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.btnOcultar);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(470, 529);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Ocultar";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // btnTexto
+            // 
+            this.btnTexto.Location = new System.Drawing.Point(30, 138);
+            this.btnTexto.Name = "btnTexto";
+            this.btnTexto.Size = new System.Drawing.Size(75, 23);
+            this.btnTexto.TabIndex = 13;
+            this.btnTexto.Text = "Buscar";
+            this.btnTexto.UseVisualStyleBackColor = true;
+            this.btnTexto.Click += new System.EventHandler(this.btnTexto_Click);
+            // 
+            // btnImagen
+            // 
+            this.btnImagen.Location = new System.Drawing.Point(30, 67);
+            this.btnImagen.Name = "btnImagen";
+            this.btnImagen.Size = new System.Drawing.Size(75, 23);
+            this.btnImagen.TabIndex = 12;
+            this.btnImagen.Text = "Buscar";
+            this.btnImagen.UseVisualStyleBackColor = true;
+            this.btnImagen.Click += new System.EventHandler(this.btnImagen_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 17);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Imagen";
+            // 
+            // txtImagen
+            // 
+            this.txtImagen.Enabled = false;
+            this.txtImagen.Location = new System.Drawing.Point(130, 68);
+            this.txtImagen.Name = "txtImagen";
+            this.txtImagen.Size = new System.Drawing.Size(294, 22);
+            this.txtImagen.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Texto";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 410);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Contraseña";
+            // 
+            // txtTexto
+            // 
+            this.txtTexto.Enabled = false;
+            this.txtTexto.Location = new System.Drawing.Point(130, 139);
+            this.txtTexto.Name = "txtTexto";
+            this.txtTexto.Size = new System.Drawing.Size(294, 22);
+            this.txtTexto.TabIndex = 3;
+            // 
+            // rtbTextoOculto
+            // 
+            this.rtbTextoOculto.EnableAutoDragDrop = true;
+            this.rtbTextoOculto.Location = new System.Drawing.Point(30, 179);
+            this.rtbTextoOculto.Name = "rtbTextoOculto";
+            this.rtbTextoOculto.ReadOnly = true;
+            this.rtbTextoOculto.Size = new System.Drawing.Size(394, 208);
+            this.rtbTextoOculto.TabIndex = 2;
+            this.rtbTextoOculto.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(130, 405);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(148, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // btnOcultar
+            // 
+            this.btnOcultar.Location = new System.Drawing.Point(179, 477);
+            this.btnOcultar.Name = "btnOcultar";
+            this.btnOcultar.Size = new System.Drawing.Size(99, 34);
+            this.btnOcultar.TabIndex = 0;
+            this.btnOcultar.Text = "Ocultar";
+            this.btnOcultar.UseVisualStyleBackColor = true;
+            this.btnOcultar.Click += new System.EventHandler(this.btnOcultar_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.btnImagen2);
+            this.tabPage2.Controls.Add(this.txtImagen2);
+            this.tabPage2.Controls.Add(this.label6);
+            this.tabPage2.Controls.Add(this.rtbRecuperar);
+            this.tabPage2.Controls.Add(this.textBox6);
+            this.tabPage2.Controls.Add(this.btnRecuperar);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(470, 529);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Recuperar";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(35, 37);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 17);
+            this.label4.TabIndex = 22;
+            this.label4.Text = "Imagen";
+            // 
+            // btnImagen2
+            // 
+            this.btnImagen2.Location = new System.Drawing.Point(38, 72);
+            this.btnImagen2.Name = "btnImagen2";
+            this.btnImagen2.Size = new System.Drawing.Size(75, 23);
+            this.btnImagen2.TabIndex = 21;
+            this.btnImagen2.Text = "Buscar";
+            this.btnImagen2.UseVisualStyleBackColor = true;
+            this.btnImagen2.Click += new System.EventHandler(this.btnImagen2_Click);
+            // 
+            // txtImagen2
+            // 
+            this.txtImagen2.Location = new System.Drawing.Point(138, 72);
+            this.txtImagen2.Name = "txtImagen2";
+            this.txtImagen2.Size = new System.Drawing.Size(294, 22);
+            this.txtImagen2.TabIndex = 20;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(35, 116);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(81, 17);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Contraseña";
+            // 
+            // rtbRecuperar
+            // 
+            this.rtbRecuperar.EnableAutoDragDrop = true;
+            this.rtbRecuperar.Location = new System.Drawing.Point(38, 229);
+            this.rtbRecuperar.Name = "rtbRecuperar";
+            this.rtbRecuperar.ReadOnly = true;
+            this.rtbRecuperar.Size = new System.Drawing.Size(394, 277);
+            this.rtbRecuperar.TabIndex = 14;
+            this.rtbRecuperar.Text = "";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(138, 113);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(148, 22);
+            this.textBox6.TabIndex = 13;
+            // 
+            // btnRecuperar
+            // 
+            this.btnRecuperar.Location = new System.Drawing.Point(175, 171);
+            this.btnRecuperar.Name = "btnRecuperar";
+            this.btnRecuperar.Size = new System.Drawing.Size(99, 34);
+            this.btnRecuperar.TabIndex = 12;
+            this.btnRecuperar.Text = "Recuperar";
+            this.btnRecuperar.UseVisualStyleBackColor = true;
+            this.btnRecuperar.Click += new System.EventHandler(this.btnRecuperar_Click);
+            // 
+            // ofdImagen
+            // 
+            this.ofdImagen.FileName = "openFileDialog1";
+            this.ofdImagen.Filter = "PNG Files|*.png|GIF Files|*.gif";
+            // 
+            // ofdTexto
+            // 
+            this.ofdTexto.FileName = "openFileDialog1";
+            this.ofdTexto.Filter = "TXT Files|*.txt";
+            // 
+            // sfdOcultar
+            // 
+            this.sfdOcultar.Filter = "PNG Files|*.png|GIF Files|*.gif";
+            // 
+            // lblCapacidad
+            // 
+            this.lblCapacidad.AutoSize = true;
+            this.lblCapacidad.ForeColor = System.Drawing.Color.Crimson;
+            this.lblCapacidad.Location = new System.Drawing.Point(35, 443);
+            this.lblCapacidad.Name = "lblCapacidad";
+            this.lblCapacidad.Size = new System.Drawing.Size(0, 17);
+            this.lblCapacidad.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(536, 601);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "StegoPro";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtImagen;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTexto;
+        private System.Windows.Forms.RichTextBox rtbTextoOculto;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnOcultar;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnImagen2;
+        private System.Windows.Forms.TextBox txtImagen2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.RichTextBox rtbRecuperar;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button btnRecuperar;
+        private System.Windows.Forms.OpenFileDialog ofdImagen;
+        private System.Windows.Forms.Button btnImagen;
+        private System.Windows.Forms.Button btnTexto;
+        private System.Windows.Forms.OpenFileDialog ofdTexto;
+        private System.Windows.Forms.SaveFileDialog sfdOcultar;
+        private System.Windows.Forms.Label lblCapacidad;
+    }
+}
+
---
+++ StegoPrograma/Form1.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text;
+
+namespace StegoPrograma
+{
+    public partial class Form1 : Form
+    {
+        Bitmap img;
+        byte[] data;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnImagen_Click(object sender, EventArgs e)
+        {
+            if (ofdImagen.ShowDialog() == DialogResult.OK)
+            {
+                txtImagen.Text = ofdImagen.FileName;
+                img = new Bitmap(txtImagen.Text);
+            }
+            lblCapacidad.Text = "Capadidad máxima del caracteres: " + (((img.Height * img.Width) - 1) / 3);
+        }
+
+        private void btnTexto_Click(object sender, EventArgs e)
+        {
+            if (ofdTexto.ShowDialog() == DialogResult.OK)
+            {
+                txtTexto.Text = ofdTexto.FileName;
+                using (FileStream fs = File.OpenRead(txtTexto.Text))
+                {
+                    data = new BinaryReader(fs).ReadBytes((int)fs.Length);               
+                }
+                rtbTextoOculto.Text = Encoding.UTF8.GetString(data);
+            }
+        }
+
+        private void btnImagen2_Click(object sender, EventArgs e)
+        {
+            if (ofdImagen.ShowDialog() == DialogResult.OK)
+            {
+                txtImagen2.Text = ofdImagen.FileName;
+                img = new Bitmap(txtImagen2.Text);
+            }
+        }
+
+        private void btnOcultar_Click(object sender, EventArgs e)
+        {
+
+            //string path = txtTexto.Text;
+            //Bitmap img = new Bitmap(txtImagen.Text);
+            //img = new Bitmap(txtImagen.Text);
+            //byte[] data;
+            int indice = 0, bit = 0;
+            int rojo = 0, verde = 0, azul = 0;
+            byte[] arr_letra_bits = new byte[8];
+            bool terminado = false;
+            /*using (FileStream fs = File.OpenRead(path))
+            {
+                data = new BinaryReader(fs).ReadBytes((int)fs.Length);
+            }*/
+
+            //data.[data.Length] = 255;
+            
+            for (int i = 0; i < img.Width; i++)
+            {
+                for (int j = 0; j < img.Height; j++)
+                {
+                    if (bit == 0 && indice < data.Length)
+                    {
+                        arr_letra_bits = obtener_bits(data[indice]);//La letra en bits
+                        Console.WriteLine("Ingresando {0} ", Convert.ToChar(data[indice]));
+                    }
+                    else if (bit == 0 && indice == data.Length)
+                    {
+                        arr_letra_bits = new byte[] { 1, 1, 1, 1, 1, 1, 1, 1 };//La letra en bits
+                        Console.WriteLine("Ingresando Char Final");
+                    }
+                    else if(indice > data.Length)
+                    {
+                        Console.WriteLine("Finalizado");
+                        terminado = true;
+                        break;
+                    }
+                       
+                    Color pixel = img.GetPixel(i, j);
+                    Console.WriteLine("Pixel Antes {0}-{1}: {2}, {3}, {4}", i, j, pixel.R, pixel.G, pixel.B);
+
+                    #region LBS Rojo
+                    if (bit < arr_letra_bits.Length)
+                    {
+                        rojo = modificar_color(pixel, 'R', arr_letra_bits[bit]);
+                        bit++;
+                    }
+                    else
+                    {
+                        bit = 0;
+                    }
+                    #endregion
+
+                    #region LBS VERDE
+                    if (bit < arr_letra_bits.Length)
+                    {
+                        verde = modificar_color(pixel, 'G', arr_letra_bits[bit]);
+                        bit++;
+                    }
+                    else
+                    {
+                        bit = 0;
+                    }
+                    #endregion
+
+                    #region LBS AZUL
+                    if (bit < arr_letra_bits.Length)
+                    {
+                        azul = modificar_color(pixel, 'B', arr_letra_bits[bit]);
+                        bit++;
+                    }
+                    else
+                    {
+                        indice++;
+                        bit = 0;
+                        azul = pixel.B;
+                    }
+                    #endregion
+
+                    Console.WriteLine("Pixel Despues {0}-{1}: {2}, {3}, {4}", i, j, rojo, verde, azul);
+                   
+                    img.SetPixel(i, j, Color.FromArgb(rojo, verde, azul));//Ingreso del pixel modificado en la imagen                  
+                }
+
+                if (terminado)
+                    break;
+            }
+            Console.WriteLine("Todo ok");
+
+            string pathEncoded = "";
+            if (sfdOcultar.ShowDialog() == DialogResult.OK)
+            {
+                pathEncoded = sfdOcultar.FileName;
+                img.Save(pathEncoded);
+            }
+
+            Console.WriteLine("Guardado");
+        }
+
+        private void btnRecuperar_Click(object sender, EventArgs e)
+        {
+            //Bitmap img = new Bitmap(txtImagen2.Text);
+            string mensaje = "", cadena_bytes = "", cadena_anterior = "";
+            byte rojo = 0, verde = 0, azul = 0;
+            for (int i = 0; i < img.Width; i++)
+            {
+                for (int j = 0; j < img.Height; j++)
+                {
+                    ///Char de finalizacion 
+                    if (cadena_anterior.Equals("11111111"))
+                        break;
+                    Color pixel = img.GetPixel(i, j);
+                    Console.WriteLine("Pixel Antes {0}-{1}: {2}, {3}, {4}", i, j, pixel.R, pixel.G, pixel.B);
+                    rojo = obtener_ultimo_bit(pixel, 'R');
+                    cadena_bytes += rojo;
+
+                    verde = obtener_ultimo_bit(pixel, 'G');
+                    cadena_bytes += verde;
+
+                    if (cadena_bytes.Length >= 8)
+                    {
+                        cadena_anterior = cadena_bytes;    
+                        if (cadena_anterior.Equals("11111111"))
+                            continue;
+                        else 
+                            mensaje += obtener_letra(cadena_bytes);
+                        cadena_bytes = "";
+
+                    }
+                    else 
+                    {
+                        azul = obtener_ultimo_bit(pixel, 'B');
+                        cadena_bytes += azul;
+                    }   
+                }
+                if (cadena_anterior.Equals("11111111"))
+                    break;
+            }
+            rtbRecuperar.Text = mensaje;
+        }
+
+        public int modificar_color(Color pixel, char color, byte bit)
+        {
+            /**Se pasa 3 parametros: el pixel, el color a cambiar y el bit que se va a cambiar.
+            Devuelve el color trasformado en entero.
+            Una vez que identifica el color a cambiar hace un arreglo que contiene bits y 
+            finalmente se hace el cambio del ultimo bit y retorna el binario ya trasformado.*/
+
+            byte[] cambio_color = new byte[8];
+            if (color.Equals('R'))
+                cambio_color = obtener_bits((byte)pixel.R);
+            else if (color.Equals('G'))
+                cambio_color = obtener_bits((byte)pixel.G);
+            else if (color.Equals('B'))
+                cambio_color = obtener_bits((byte)pixel.B);
+
+            cambio_color[7] = bit;
+
+            int nuevo_color = Convert.ToInt32(obtener_byte(cambio_color));
+            return nuevo_color;
+        }
+
+        public byte[] obtener_bits(byte valor)
+        {
+            /**Se pasa 1 parametro: el byte del pixel.
+            Devuelve un arreglo de bits.
+            Trasforma el byte en bits y lo pone en un arreglo de bytes.*/
+            BitArray arr_bits = new BitArray(new byte[] { valor });
+            bool[] arr_booleanos = new bool[arr_bits.Count];
+            arr_bits.CopyTo(arr_booleanos, 0);
+            byte[] bitsArray = arr_booleanos.Select(bit => (byte)(bit ? 1 : 0)).ToArray();
+            Array.Reverse(bitsArray);
+
+            return bitsArray;
+        }
+
+        public byte obtener_byte(byte[] bits)
+        {
+            /**Se pasa 1 parametro: el arreglo de bits.
+             Devuelve un byte en forma de entero
+            Convierte el arreglo de bits en una cadena que será luego
+            trasformada en un byte*/
+            string cadena_bits = "";
+            for (int i = 0; i < bits.Length; i++)
+                cadena_bits += bits[i];
+            byte bits_unidos = Convert.ToByte(cadena_bits, 2);
+            return bits_unidos;
+        }
+
+        public byte obtener_ultimo_bit(Color pixel, char color)
+        {
+            /**Se pasa 2 parametros: el pixel y el color a operar.
+            Devuelve un bit del arreglo de byte
+            Verifica cual color se esta trabajando y en base a ese 
+            obtiene un arreglo de bits*/
+            byte[] arr_bytes = new byte[8];
+            if (color.Equals('R'))
+                arr_bytes = obtener_bits((byte)pixel.R);
+            else if (color.Equals('G'))
+                arr_bytes = obtener_bits((byte)pixel.G);
+            else if (color.Equals('B'))
+                arr_bytes = obtener_bits((byte)pixel.B);
+
+            return arr_bytes[7];
+        }
+
+        public string obtener_letra(string cadena)
+        {
+            /**Se pasa 1 parametro: un string que contiene un binario.
+            Devuelve el caracter asociado con el binario
+            Convierte el string para luego convertirlo en un caracter*/
+            byte[] bytes = new byte[1];
+
+            bytes[0] = Convert.ToByte(cadena, 2);
+
+            string resultado = Encoding.UTF8.GetString(bytes);
+            return resultado;
+        }
+    }
+}
